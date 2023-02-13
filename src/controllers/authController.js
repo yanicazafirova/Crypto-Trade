aa2d518cc6cfa4ddca15bf39da3e85cdd28a2b53
Yanica Zafirova <yanica_zafirova@abv.bg>
2023-02-13 16:13:35 +0200

    Create authentication controller

---
+++ src/controllers/authController.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+
+const authService = require('../services/authService');
+const { getErrorMessage } = require('../utils/errorMessage');
+const { AUTH_COOKI_NAME } = require('../constants');
+
+const { isAuth } = require('../middlewares/authMiddleware');
+
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+router.post('/register', async (req, res) => {
+    const { username, email, password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+        res.locals.error = 'Passwords or email dont match!';
+        return res.render('auth/register');
+    }
+
+    try {
+        const user = await authService.register({ username, email, password });
+        const token = await authService.getSession(user);
+        //TODO: Should add token
+        res.cookie(AUTH_COOKI_NAME, token, { httpOnly: true });
+
+        res.redirect('/');
+    } catch (error) {
+        res.render('auth/register', { error: getErrorMessage(error) });
+    }
+});
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const token = await authService.login(email, password);
+        res.cookie(AUTH_COOKI_NAME, token, { httpOnly: true });
+
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+        res.render('auth/login', { error: error.message });
+    }
+});
+
+router.get('/logout', isAuth, (req, res) => {
+    res.clearCookie(AUTH_COOKI_NAME);
+    res.redirect('/');
+});
+
+module.exports = router;
