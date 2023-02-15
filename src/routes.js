b8f48f2acacd214e9337813ef155d4ae11a51b02
Yanica Zafirova <yanica_zafirova@abv.bg>
2023-02-15 12:29:06 +0200

    Add crypto controller to routes

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const authController = require('./controllers/authController');
-//const cryptoController = require('./controllert cryptoController');
+const cryptoController = require('./controllers/cryptoController');
 
 router.use(homeController);
 router.use('/auth', authController);
-//router.use('/crypto', cryptoController);
+router.use('/crypto', cryptoController);
 router.get('/*', (req, res) => {
     res.render('home/404');
 });
